439cb5e81e0afa8088a6f077efdf7846d55205a7
John Codeinaire <johncodeinaire@gmail.com>
2019-10-30 10:28:16 +1100

    Add create stream endpoint & clean up naming convention

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "nmm_client_logging_invoke_url" {
-  value = "${aws_api_gateway_deployment.nmm_client_logging.invoke_url}prod${aws_api_gateway_resource.nmm_client_logging_resource.path}"
+output "nmm_client_put_logs_url" {
+  value = "${aws_api_gateway_deployment.nmm_client_logging_common.invoke_url}prod${aws_api_gateway_resource.nmm_client_put_logs.path}"
+}
+
+output "nmm_client_create_stream_url" {
+  value = "${aws_api_gateway_deployment.nmm_client_logging_common.invoke_url}prod${aws_api_gateway_resource.nmm_client_create_streams.path}"
 }
 
